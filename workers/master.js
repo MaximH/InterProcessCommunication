eb7f9d5a6a626a16f4bae597a018b09034221ac1
- <->
2016-03-24 11:56:43 +0200

    updtmaster

--- workers/master.js
+++ workers/master.js
@@ -1,20 +1,28 @@
 module.exports = function() {
 
+  var task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11];
+  var results = [];
+  var metaarr = [];
+  
   var cpuCount = api.os.cpus().length;
-
+  
   var workers = [];
   for (var i = 0; i < cpuCount; i++) {
     var worker = api.cluster.fork();
     workers.push(worker);
   }
 
-  var task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11];
-  var results = [];
-
+  var numofelem = task.length/cpuCount;
+  metaarr[0] = task.slice(0,numofelem);
+  metaarr[1] = task.slice(numofelem,task.length);
+  var i =0;
+  
   workers.forEach(function(worker) {
+	  
+	  
 
-    worker.send({ task: task });
-
+    worker.send({ task: metaarr[i] });
+	++i;
     worker.on('exit', function (code) {
       console.log('exit ' + worker.process.pid + ' ' + code);
     });
@@ -24,9 +32,11 @@ module.exports = function() {
         'message from worker ' + worker.process.pid + ': ' +
         JSON.stringify(message)
       );
-      results = Array.prototype.slice.call(message.result);
-	  console.log(results);
 
+	  for(var i = 0; i < message.result.length;i++) results.push(message.result[i]);
+
+	  console.log(results);
+	   
       if (results.length === cpuCount) {
         process.exit(1);
       }
